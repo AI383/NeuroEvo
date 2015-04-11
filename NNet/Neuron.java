23f59263f80c235e3d2fbdae9807403493f5e50f
patbeagan1 <theomgpat@gmail.com>
2015-04-11 17:18:06 -0400

    changes to neuron

--- NNet/Neuron.java
+++ NNet/Neuron.java
@@ -2,7 +2,6 @@ import java.util.Random;
 
 public class Neuron{
 
-	private float error;
 	private int numInputs;
 	private double[] weightMatrix = new double[];
 	private double output;
@@ -19,15 +18,11 @@ public class Neuron{
 //Actives----------------------------------------------
 
 	void activateSigmoid (){}
-	//Performs a sigmoid activation on a neuron's input and computes its output.
-
 	void computeError (float val){}
-	//Computes the output error of a neuron in an output neuron layer
-	//by comparing the neuron's current output with the targetValue.
 
 //Getters---------------------------------------
 
 	float getNumInputs(){return numInputs}
 	float getOutput(){return output}
-	float getError(){return error}
+
 }
